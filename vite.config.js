5d06b8e6d7ea15f8d42d29e94f9b346c31319537
Gokce227 <zumral.gokce@gmail.com>
2025-05-31 18:23:34 +0300

    Fix router and build settings for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Rick-and-Morty-Table/', // Projenin yolu
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
+  base: '/Rick-and-Morty-Table/',
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',
+  }
 })
