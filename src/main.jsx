4f4088c1ab26be3a2086e660b544ae71f4c17cb9
Gokce227 <zumral.gokce@gmail.com>
2025-05-31 18:26:36 +0300

    fix: deploy for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </StrictMode>
 )
