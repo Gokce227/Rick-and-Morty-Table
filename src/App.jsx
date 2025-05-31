c4d94c2416f4e89c14d3aaa3b2e36d5030041cd7
Gokce227 <zumral.gokce@gmail.com>
2025-05-31 18:30:30 +0300

    deploy için build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import EpsidoTable from './components/EpsidoTable'
 
-
 function App() {
   return (
-    // Ilk Karakter sayfalari gozukecek
-    <Router>
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/epsido" element={<EpsidoTable />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/epsido" element={<EpsidoTable />} />
         </Routes>
       </div>
-    </Router>
   )
 }
 
